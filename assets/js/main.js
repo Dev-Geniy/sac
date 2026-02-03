3125234e61db91df1463e3fab7261e8619eb4710
Developer Evgeniy <79357491+Dev-Geniy@users.noreply.github.com>
2026-02-03 20:53:16 +0200

    Add mobile navigation toggle functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,6 @@
+const burger = document.getElementById("burger");
+const navMobile = document.getElementById("navMobile");
 
+burger.addEventListener("click",()=>{
+navMobile.classList.toggle("show");
+});
